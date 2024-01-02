3b5680cb385a43f0bf5a6aa12ed6de259a49b16f
Abhishu <donabhishu@gmail.com>
2024-01-02 16:04:18 +0545

    Login and signup

---
+++ frontend/src/components/login/login.jsx
@@ -0,0 +1,67 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import img from '../../assets/login.jpg'
+import {Link, useNavigate} from 'react-router-dom'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from '../../redux/authSlice.js'
+import css from './css.css'
+const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(false)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogin = async(e) => {
+    e.preventDefault()
+
+    if(email === '' || password === '') return
+
+    try {
+      const res = await fetch(`http://localhost:5000/auth/login`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({email, password})
+      })
+
+      const data = await res.json()
+
+      dispatch(login(data))
+      navigate('/')
+    } catch (error) {
+      setError(true)
+      setTimeout(() => {
+        setError(false)
+      }, 3000)
+    }
+  }
+
+  return (
+    <div className={css.loginContainer}>
+      <div className={css.loginWrapper}>
+          <div className={css.loginLeftSide}>
+            <img src={img} className={css.leftImg}/>
+          </div>
+          <div className={css.loginRightSide}>
+            <h2 className={css.title}>Login</h2>
+            <form onSubmit={handleLogin} className={css.loginForm}>
+               <input type="email" placeholder='Type email' onChange={(e) => setEmail(e.target.value)}/>
+               <input type="password" placeholder='Type password' onChange={(e) => setPassword(e.target.value)}/>
+               <button className={css.submitBtn}>Login</button>
+               <p>Don't have an account? <Link to='/signup'>Sign up</Link></p>
+            </form>
+            {error && (
+              <div className={css.errorMessage}>
+                Wrong credentials! Try different ones
+              </div>
+            )}
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
